7467bf20e11890f2c1868d2925deab9c6a69a070
Dmytro Orikhovskyi <dimaoag@gmail.com>
2021-09-02 14:13:25 +0300

    flash messages and url generator

--- api/src/Controller/AboutController.php
+++ api/src/Controller/AboutController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 #[Route('/about', name: 'about_')]
 final class AboutController extends AbstractController
@@ -31,9 +32,20 @@ final class AboutController extends AbstractController
         locale: 'en',
         format: 'html',
     )]
-    public function show(int $id, Request $request): Response
+    public function show(int $id, Request $request, UrlGeneratorInterface $route): Response
     {
         $allAttributes = $request->attributes->all();
+        $url = $route->generate(
+            'about_index',
+            [],
+            UrlGeneratorInterface::ABSOLUTE_URL
+        );
+
+        $this->addFlash(
+            'notice',
+            'Ваши изменения сохранены!'
+        );
+        // $this->addFlash() is equivalent to $request->getSession()->getFlashBag()->add()
 
 
         return $this->render('base.html.twig', [
