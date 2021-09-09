9ae252a59a7b09984b59b150d3601b49955b1704
Dmytro Orikhovskyi <dimaoag@gmail.com>
2021-09-09 17:11:25 +0300

    events

---
+++ api/src/Service/SomeTest/EventListener/ExceptionListener.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\SomeTest\EventListener;
+
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+final class ExceptionListener
+{
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        // Вы получаете объект исключения из полученного события
+        $exception = $event->getThrowable();
+        $message = sprintf(
+            'My Error says: %s with code: %s',
+            $exception->getMessage(),
+            $exception->getCode()
+        );
+
+        // Настройте ваш объект ответа, чтобы он отображал детали исключений
+        $response = new Response();
+        $response->setContent($message);
+
+        // HttpExceptionInterface - это специальный тип исключения, который
+        // содержит статус кода и детали заголовка
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+            $response->headers->replace($exception->getHeaders());
+        } else {
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        // Отправляет изменённый объект ответа событию
+        $event->setResponse($response);
+    }
+}
---
+++ api/src/Service/SomeTest/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\SomeTest\EventSubscriber;
+
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+final class LocaleSubscriber implements EventSubscriberInterface
+{
+    private string $defaultLocale;
+
+    public function __construct(?string $defaultLocale = 'en')
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        // попробуйте увидеть, была ли локаль установлена как параметр маршрутизации _locale
+        if ($locale = $request->attributes->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+        } else {
+            // если для этого запроса не было ясно установлено никакой локали, используйте её из сесии
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            // должен быть зарегистрирован после слушателя локали по умолчанию
+            KernelEvents::REQUEST => [['onKernelRequest', 15]],
+        ];
+    }
+}
